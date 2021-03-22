144b1298f5ecf19a2ac4b883a2905e574965ad28
Fareman <misha.xarun2011@mail.ru>
2021-03-22 11:15:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Polibiya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 216);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(330, 103);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(215, 216);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(588, 103);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 216);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Дешифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Зашифрованный текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(599, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Расшифрованный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(326, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Нераспознынные символы: ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(549, 350);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 389);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Polibiya
+{
+    public partial class Form1 : Form
+    {
+        string input;
+        int one;
+        int two;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char[,] alphabet = new char[8, 9]
+            {
+                {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з'},
+                {'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р'},
+                {'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'},
+                {'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В'},
+                {'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К'},
+                {'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У'},
+                {'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь'},
+                {'Э', 'Ю', 'Я', ' ', '.', ':', '!', '?', ','},
+            };
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = textBox1.Text;   //чтение текста
+            int countMessage = message.Length;  //подсчет длины строки (символы)
+            char[] b = new char[countMessage];  //массив под разделенные символы
+            for (int i = 0; i < countMessage; i++)  //заполнение массива символами
+            {
+                b[i] = message[i];
+            }
+            input = "";
+            for (int a = 0; a < b.Length; a++)  //проверка по количеству введеных символов
+            {
+                for (int i = 0; i < alphabet.GetLength(0); i++)
+                {
+                    for (int j = 0; j < alphabet.GetLength(1); j++)
+                    {
+                        if (b[a] == alphabet[i, j])  //проверка соответствия символов
+                        {
+                            input += (i + 1) + "" + (j + 1) + " "; //вывод результата
+                        }
+                        else
+                        {
+                            checkBox1.Checked = true;
+                        }
+                    }
+                }
+                textBox2.Text = input.ToString();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message2 = textBox2.Text;
+            int countCryptMessage = message2.Length - 1;  //подсчет длины зашифрованной строки
+            textBox3.Clear();
+            for (int a = 0; a < countCryptMessage; a++)
+            {
+                one = int.Parse(message2[a].ToString());  //получение чисел для поиска буквы в алфавите
+                a++;
+                two = int.Parse(message2[a].ToString());
+                textBox3.Text += alphabet[one - 1, two - 1].ToString();  //"-" потому что массив начинается с 0, а данные у нас кодируются с 1
+                a++;
+            }
+
+        }
+    }
+}
